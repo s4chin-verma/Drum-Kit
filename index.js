4aa5f7b1a1fdb1b8a6635bfec6db872cad9fb1d8
Sachin Verma <101526717+s4chin-verma@users.noreply.github.com>
2023-04-19 04:23:00 +0530

    Just Deploying Drum Kit 😁

---
+++ index.js
@@ -0,0 +1,56 @@
+var length = document.querySelectorAll(".drum").length;
+for (var i = 0; i < length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var target = this.innerHTML;
+        make_sound(target);
+        animation(target);
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    make_sound(event.key);
+    animation(event.key);
+});
+
+function make_sound(key) {
+    switch (key) {
+        case "w":
+            var audio1 = new Audio("sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio("sounds/tom-2.mp3");
+            audio2.play();
+            break;
+        case "s":
+            var audio3 = new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+        case "d":
+            var audio4 = new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+        case "j":
+            var audio5 = new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio("sounds/crash.mp3");
+            audio6.play();
+            break;
+        case "l":
+            var audio7 = new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break;
+        default:
+            console.log(key);
+    }
+}
+
+function animation(current_key) {
+    var active_button = document.querySelector("." + current_key);
+    active_button.classList.add("pressed");
+    setTimeout(function () {
+        active_button.classList.remove("pressed");
+    }, 100);
+}
